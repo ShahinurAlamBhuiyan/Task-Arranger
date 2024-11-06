e1244b8fbfb4023c51560f0f6e5bb48035954df8
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-11-06 12:03:24 +0600

    setup shadcn & clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-     Let's build task-arranger
+      Let's build task-arranger
+
+      <Button>Add me</Button>
     </div>
   );
 }
